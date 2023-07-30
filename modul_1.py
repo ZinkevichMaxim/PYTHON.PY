06e980e5d24f79f7fea9a3571ee3b731021db273
ZINKEVICH Maxim <drmvz@mail.ru>
2023-07-30 14:23:22 +0300

    \fin

---
+++ modul_1.py
@@ -0,0 +1,11 @@
+# def max_1(a, b):
+#     if a > b:
+#         return(a)
+#     else:
+#         return(b)
+#     return прерывает функцию следовательно else полюбому не сработает. Поэтому можно убрать
+
+def max_1(a, b):
+    if a > b:
+        return(a)
+    return(b)
